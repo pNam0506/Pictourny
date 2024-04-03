1170a0b5e3a53937235d51337932c08a9d710c89
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2024-04-03 08:40:02 +0700

    database

---
+++ database_pictourny.sql
@@ -0,0 +1,12 @@
+create database Pictourny;
+use Pictourny;
+create table Login(
+	users_Email varchar(50),
+    users_Password varchar(50)
+);
+create table Sign_Up(
+	users_Name varchar(50),
+	users_Email varchar(50),
+    users_Password varchar(50)
+);
+select * from Sign_Up;
