5147f2ade692b6d828e5d3e8c5dae6e6e65e4bad
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2024-04-03 12:41:38 +0700

    เชื่อมต่อกับ MySQL

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = 'localhost';
+$port = 3306;
+$dbname = 'pictourny';
+$username = 'pictourny';
+$password = '1234';
+
+// Create a mysqli connection
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+}
+
+echo 'Connected successfully';
+
+?>
