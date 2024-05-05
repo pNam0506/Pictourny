2439225069dbbfb11c4a01632a24356118054efd
realsky146 <see070801@gmail.com>
2024-05-06 02:30:08 +0700

    ส่งงานจู้ว

---
+++ Pictourny.js
@@ -0,0 +1,32 @@
+
+$(Pictourny).ready(() => {
+          $('.gallery').slick({
+              centerMode: true,
+              centerPadding: '60px',
+              slidesToShow: 3,
+              responsive: [
+                  {
+                      breakpoint: 768,
+                      settings: {
+                          arrows: false,
+                          centerMode: true,
+                          centerPadding: '40px',
+                          slidesToShow: 3
+                      }
+                  },
+                  {
+                      breakpoint: 480,
+                      settings: {
+                          arrows: false,
+                          centerMode: true,
+                          centerPadding: '40px',
+                          slidesToShow: 1
+                      }
+                  }
+              ]
+          });
+      });
+
+        function goToNextPage(url) {
+        window.location.href = url;
+}
