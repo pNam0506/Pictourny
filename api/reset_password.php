20a5e02702b30bcecc1323d2b9c5ad29f263f1e6
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2025-01-06 23:01:26 +0700

    Update reset_password.php

--- api/reset_password.php
+++ api/reset_password.php
@@ -16,7 +16,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once(__DIR__ . 'config.php');
+require_once(__DIR__ . './config.php');
 
 require_once(__DIR__ . './phpmailer/vendor/autoload.php');
 
